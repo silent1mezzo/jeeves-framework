79929355fea479390498353c0251d4c5891559e1
Adam McKerlie <adammckerlie@gmail.com>
2012-04-09 16:17:43 -0400

    Fixed #10

---
+++ jeeves/tests/plugin_test.py
@@ -0,0 +1,45 @@
+"""
+    Example Plugins used for Testing
+"""
+
+from jeeves.core.plugin import CommandPlugin, GenericPlugin
+
+class ExampleGenericPlugin(GenericPlugin):
+    name = 'Example Generic Plugin'
+    help_text = 'This is the help text for a generic plugin.'
+
+    def __init__(self, *args, **kwargs):
+        super(ExampleGenericPlugin, self).__init__(self.name, *args, **kwargs)
+
+    def handle_message(self, channel, message):
+        self.say(channel, "I am a Generic Plugin")
+
+class ExampleCommandPlugin(CommandPlugin):
+    name = 'Example Command Plugin'
+    help_text = 'This is the help text for a command plugin.'
+    command = 'example'
+
+    def __init__(self, *args, **kwargs):
+        super(ExampleCommandPlugin, self).__init__(self.name, self.command, *args, **kwargs)
+
+    def handle_message(self, channel, message):
+        self.say(channel, "I am a Command Plugin")
+
+class ExampleNoNamePlugin(GenericPlugin):
+    help_text = 'This is the help text for a generic plugin.'
+
+    def __init__(self, *args, **kwargs):
+        super(ExampleNoNamePlugin, self).__init__(*args, **kwargs)
+
+    def handle_message(self, channel, message):
+        self.say(channel, "I am a Generic Plugin")
+
+class ExampleNoCommandPlugin(CommandPlugin):
+    name = 'Example Command Plugin'
+    help_text = 'This is the help text for a command plugin.'
+
+    def __init__(self, *args, **kwargs):
+        super(ExampleCommandPlugin, self).__init__(self.name, *args, **kwargs)
+
+    def handle_message(self, channel, message):
+        self.say(channel, "I am a Command Plugin")
--- jeeves/utils/utils.py
+++ jeeves/utils/utils.py
@@ -1,4 +1,9 @@
 from jeeves.conf import settings
+
+def pop_message(lst, item):
+    index = lst.index(item)
+    lst.pop(index)
+    return lst
 '''
     Parses a given message directed at the bot.
 
@@ -10,21 +15,25 @@ from jeeves.conf import settings
         Command - Command used for a given plugin
         Query - Passed to the plugin
 '''
-def parse_message(msg, commands):
-    msg = msg.lower()
+def parse_message(message, commands):
     nick = settings.NICKNAME.lower()
     plugin_command = None
+    tokenized_msg = message.lower().split()
 
-    index = msg.find(nick)
-    if index != -1:
-        nickname = msg[index:len(nick)]
-        msg = msg.replace('%s:' % nick, '')
-        msg = msg.replace(nick, '')
+    if nick == tokenized_msg[0] or '%s:' % nick == tokenized_msg[0]:
+        nickname = nick
+        try:
+            tokenized_msg = pop_message(tokenized_msg, nick)
+        except ValueError:
+            # Happens when users calls bot with `BotName: msg`
+            tokenized_msg = pop_message(tokenized_msg, '%s:' % nick)
     else:
         nickname = None
 
     for command in commands:
-        if msg.find(command.lower()) != -1:
+        if command.lower() == tokenized_msg[0]:
             plugin_command = command
+            tokenized_msg = pop_message(tokenized_msg, command.lower())
 
-    return (nickname, plugin_command, msg)
+    message = ' '.join(tokenized_msg)
+    return (nickname, plugin_command, message)
