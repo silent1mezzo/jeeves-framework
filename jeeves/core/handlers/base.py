55722bfc050c45f2ecd62ac95ae5f71791183b01
Adam McKerlie <adammckerlie@gmail.com>
2012-04-11 17:27:24 -0400

    Erroneous plugin will no longer cause Jeeves to stall

--- jeeves/core/handlers/base.py
+++ jeeves/core/handlers/base.py
@@ -17,33 +17,39 @@ class BaseHandler(object):
 
         for plugin_path in settings.PLUGINS:
             try:
-                pg_module, pg_classname = plugin_path.rsplit('.', 1)
-            except ValueError, e:
-                raise exceptions.ImproperlyConfigured('%s isn\'t a plugin module' % plugin_path)
+                try:
+                    pg_module, pg_classname = plugin_path.rsplit('.', 1)
+                except ValueError, e:
+                    raise exceptions.ImproperlyConfigured('%s isn\'t a plugin module' % plugin_path)
 
-            try:
-                mod = importlib.import_module(pg_module)
-            except ImportError, e:
-                raise exceptions.ImproperlyConfigured('Error importing plugin %s: "%s"' % (pg_module, e))
+                try:
+                    mod = importlib.import_module(pg_module)
+                except ImportError, e:
+                    raise exceptions.ImproperlyConfigured('Error importing plugin %s: "%s"' % (pg_module, e))
 
-            try:
-                pg_class = getattr(mod, pg_classname)
-            except AttributeError, e:
-                raise exceptions.ImproperlyConfigured('Plugins module "%s" does not define a "%s" class' % (pg_module, pg_classname))
+                try:
+                    pg_class = getattr(mod, pg_classname)
+                except AttributeError, e:
+                    raise exceptions.ImproperlyConfigured('Plugins module "%s" does not define a "%s" class' % (pg_module, pg_classname))
 
-            try:
-                pg_instance = pg_class(handler=self, protocol=self.protocol)
-            except TypeError, e:
-                raise exceptions.InvalidPlugin('Plugins class "%s" does not provide a command: %s' % (pg_class, e))
+                try:
+                    pg_instance = pg_class(handler=self, protocol=self.protocol)
+                except TypeError, e:
+                    raise exceptions.InvalidPlugin('Plugins class "%s" does not provide a command: %s' % (pg_class, e))
 
-            if hasattr(pg_instance, 'command'):
-                if isinstance(pg_instance.command, str):
-                    self._command_plugins[pg_instance.command] = pg_instance
+                if hasattr(pg_instance, 'command'):
+                    if isinstance(pg_instance.command, str):
+                        self._command_plugins[pg_instance.command] = pg_instance
+                    else:
+                        for command in pg_instance.command:
+                            self._command_plugins[command] = pg_instance
                 else:
-                    for command in pg_instance.command:
-                        self._command_plugins[command] = pg_instance
-            else:
-                self._generic_plugins.append(pg_instance)
+                    self._generic_plugins.append(pg_instance)
+            except Exception as e:
+                if pg_classname:
+                    print "%s: %s" % (pg_classname, e)
+                else:
+                    print e
 
     def message(self, user, channel, msg):
         user = user.split('!', 1)[0]
