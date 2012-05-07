5c1ae41a4fe982b6deef6a5105c181822a646fe7
Adam McKerlie <adammckerlie@gmail.com>
2012-05-07 17:17:12 -0400

    Added TicketBot to plugins. #32

--- jeeves/conf/project_template/settings.py
+++ jeeves/conf/project_template/settings.py
@@ -2,8 +2,8 @@
 
 
 HOST = 'irc.freenode.net'
-PORT = 6667 # 6697 for SSL
-SSL = False 
+PORT = 6667  # 6697 for SSL
+SSL = False
 SERVER_PASSWORD = None
 PASSWORD = None
 NICKNAME = 'Jeeves'
@@ -12,9 +12,12 @@ REALNAME = 'Jeeves Bot: A Framework for creating IRC Bots'
 CHANNEL = '#jeeves'
 
 PLUGINS = (
-    #It is strongly recommended that you don't remove these jeeves.* plugins
+    #It is strongly recommended that you don't remove these (jeeves.core.plugins.*) plugins
     'jeeves.core.plugins.admin.HelpPlugin',
-    'jeeves.core.plugins.shortener.ShortenPlugin',
+
+    #Optional Jeeves Plugins. Uncomment any you'd like to use.
+    #'jeeves.contrib.plugins.ticketbot.TicketBotPlugin',
+    #'jeeves.contrib.plugins.shortener.ShortenPlugin',
 
     #Here you can add any of the plugins that you create.
     #'plugins.example.ExampleGenericPlugin',
@@ -22,11 +25,21 @@ PLUGINS = (
 )
 
 """
-    Possible settings used for ShortenerPlugin
-"""
+###########################
+# Shorten Plugin Settings #
+###########################
 # Backend shortening service. Defaults to google
 SHORTENER_BACKEND = 'google'
 # Login account. Defaults to None
 SHORTENER_LOGIN = None
 # API Key. Defaults to None
 SHORTENER_API_KEY = None
+"""
+
+"""
+#############################
+# TicketBot Plugin Settings #
+#############################
+TICKETBOT_TICKET_URL = None  # Example: 'https://github.com/silent1mezzo/jeeves-framework/issues/%s'
+TICKETBOT_CHANGESET_URL = None  # Example: 'https://github.com/silent1mezzo/jeeves-framework/commit/%s'
+"""
